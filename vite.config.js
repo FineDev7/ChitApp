9e9761579d40ea22612c945f92d22365d9e9d5e3
FineDev7 <howareyou1771@gmail.com>
2025-06-14 21:01:40 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    base: './'
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Chit Fund App',
+        short_name: 'ChitFund',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4a90e2',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),react()],
+});
